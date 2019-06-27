56dea8515c4627afb7303b2ffdbcb2ec2367b2e8
The Omar <faroque.ae@gmail.com>
2019-06-27 17:46:08 -0500

    update song insert query

--- Project1/sql_queries.py
+++ Project1/sql_queries.py
@@ -64,7 +64,13 @@ songplay_table_insert = ("""
 user_table_insert = ("""
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs(
+song_id,
+title,
+artist_id,
+year,
+duration
+)VALUES(%s,%s,%s,%s,%s);
 """)
 
 artist_table_insert = ("""
@@ -83,3 +89,5 @@ song_select = ("""
 create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create,
                         time_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+
+insert_table_queries = [song_table_insert]
