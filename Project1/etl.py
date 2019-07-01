4f691b7b1c0d2ad4d286f7cbf0f4bedfaa33cb57
The Omar <faroque.ae@gmail.com>
2019-07-01 13:56:22 -0500

    inserting data from the pipeline code into artist and song table

--- Project1/etl.py
+++ Project1/etl.py
@@ -3,21 +3,30 @@ import glob
 import psycopg2
 import pandas as pd
 from sql_queries import *
-
+import sys
 
 def process_song_file(cur, filepath):
+    print('path of the file: '+filepath)
     # open song file
-    df = 
-
+    df = pd.read_json(filepath,lines=True)
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values
     # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
-    
+    song_data = list(song_data[0])
+    record_to_insert = (song_data[0], song_data[1], song_data[2], song_data[3], song_data[4])
+    try:
+        cur.execute(song_table_insert, record_to_insert)
+    except TypeError as error:
+        e = sys.exc_info()[0]
+        print(e)
+        print(error)
+
     # insert artist record
-    artist_data = 
-    cur.execute(artist_table_insert, artist_data)
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values
+    artist_data = list(artist_data[0])
+    artist_data_to_insert = (artist_data[0], artist_data[1], artist_data[2], artist_data[3], artist_data[4])
+    cur.execute(artist_table_insert, artist_data_to_insert)
 
-
+'''
 def process_log_file(cur, filepath):
     # open log file
     df = 
@@ -59,7 +68,7 @@ def process_log_file(cur, filepath):
         songplay_data = 
         cur.execute(songplay_table_insert, songplay_data)
 
-
+'''
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
@@ -80,11 +89,11 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=postgres password=password")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    # process_data(cur, conn, filepath='data/log_data', func=process_log_file)
 
     conn.close()
 
