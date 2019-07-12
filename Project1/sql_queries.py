58c6db45fe809bb949d1b005891feb74c104ec1e
The Omar <faroque.ae@gmail.com>
2019-07-12 17:26:36 -0500

    update the query

--- Project1/sql_queries.py
+++ Project1/sql_queries.py
@@ -78,8 +78,7 @@ last_name,
 gender,
 level
 )VALUES(%s,%s,%s,%s,%s)
-ON CONFLICT (user_id)
-DO NOTHING;
+ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
 """)
 
 song_table_insert = ("""INSERT INTO songs(
